1ffc01fc2bf2521db75df0e593b77cdb474b43c8
KrisHugo <krishugo@users.noreply.github.com>
2020-02-06 15:33:13 +0800

    酒店分页展示完成

--- src/main/java/com/LiteTravel/web/controller/HotelController.java
+++ src/main/java/com/LiteTravel/web/controller/HotelController.java
@@ -2,26 +2,44 @@ package com.LiteTravel.web.controller;
 
 import com.LiteTravel.web.mapper.HotelMapper;
 import com.LiteTravel.web.po.Hotel;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Collection;
+import java.util.List;
 
 @Controller
 public class HotelController {
     @Autowired
     public HotelMapper hotelMapper;
+    /* 默认第一页 */
     @GetMapping("/hotels")
     public String HotelList(ModelMap model){
-        Collection<Hotel> hotels = hotelMapper.getHotels();
+        setPageHotel(1, model);
+        return "hotels";
+    }
+    /* 点击页面数切换 分页显示酒店列表 */
+    @GetMapping("/hotels/{page}")
+    public String HotelPage(@PathVariable("page") Integer page, ModelMap model){
+        setPageHotel(page, model);
+        return "hotels";
+    }
+    /* 使用PageHelper获得并设置 分页数据 */
+    private void setPageHotel(Integer page, ModelMap model){
+        PageHelper.startPage(page, 3);
+        List<Hotel> hotels = hotelMapper.getHotels();
+        PageInfo<Hotel> info = new PageInfo<>(hotels, 5);
         /* 设置筛选页面的筛选项目为Hotel */
 //        model.addAttribute("category", "hotel");
         /* 放入数据 */
+        /* 放入hotel列表数据 */
         model.addAttribute("hotels", hotels);
-        return "hotels";
+        /* 放入页面信息数据 */
+        model.addAttribute("pageInfo", info);
     }
 
     @GetMapping("/hotel/{id}")
