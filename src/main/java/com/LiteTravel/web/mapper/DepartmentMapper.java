1808155634d296a05b7eb7251a35f49b740fc5f6
Kristian-Lee <1182696032@qq.com>
2020-12-11 01:47:56 +0800

    实现了订单复合查询的service层

--- src/main/java/com/LiteTravel/web/mapper/DepartmentMapper.java
+++ src/main/java/com/LiteTravel/web/mapper/DepartmentMapper.java
@@ -2,9 +2,10 @@ package com.LiteTravel.web.mapper;
 
 import com.LiteTravel.web.Model.Department;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-
+@Repository
 public interface DepartmentMapper {
     @Select("select * from department where id = #{id}")
     public Department getDepartmentById(Integer id);
