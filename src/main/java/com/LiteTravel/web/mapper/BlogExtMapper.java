345816092e2ccc0e481d0e4000f2c5e0da9a5437
Kristian-Lee <1182696032@qq.com>
2020-12-11 10:33:44 +0800

    实现了订单复合查询的service层

--- src/main/java/com/LiteTravel/web/mapper/BlogExtMapper.java
+++ src/main/java/com/LiteTravel/web/mapper/BlogExtMapper.java
@@ -1,7 +1,9 @@
 package com.LiteTravel.web.mapper;
 
 import com.LiteTravel.web.Model.Blog;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogExtMapper {
     public void incCommentCount(Blog blog);
 }
--- src/main/java/com/LiteTravel/web/mapper/LineMapper.java
+++ src/main/java/com/LiteTravel/web/mapper/LineMapper.java
@@ -1,9 +1,10 @@
 package com.LiteTravel.web.mapper;
 
 import com.LiteTravel.web.Model.Line;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface LineMapper {
     public List<Line> getAllLines();
 }
