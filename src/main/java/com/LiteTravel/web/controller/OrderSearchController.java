6bfd2d9f384b540495dd76e4cfeabd3136e3f25f
Kristian-Lee <1182696032@qq.com>
2020-12-11 13:49:40 +0800

    实现了订单复合查询的service层

--- src/main/java/com/LiteTravel/web/controller/OrderSearchController.java
+++ src/main/java/com/LiteTravel/web/controller/OrderSearchController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class SearchController {
+public class OrderSearchController {
 
     @Autowired
     SearchService searchService;
