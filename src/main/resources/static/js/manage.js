d5ce310600600429c74db9cd03b05dde093a5fb6
Colatis <55677664+Colatis@users.noreply.github.com>
2020-12-21 08:48:32 +0800

    机票服务后端数据更改未完善

--- src/main/resources/static/js/manage.js
+++ src/main/resources/static/js/manage.js
@@ -151,50 +151,91 @@ function checkBlank(value){
     return value === null || value.toString().trim().length === 0
 }
 
+
+
+
 // 后台机票
-function getFlightMessage(flightId) {
-//    post获取信息
+// 获取页面页面信息到模态框
+function getFlightMessage(flightId, companyId, seats, level, from, to, depart, arrived) {
+    $("#flightId").val(flightId);
+    $("#companyId").val(companyId);
+    $("#toSeats").val(seats);
+    $("#flightDepart").val(depart);
+    $("#flightArrived").val(arrived);
+    document.getElementById('flightFrom').innerHTML=from;
+    document.getElementById('flightTo').innerHTML=to;
+    document.getElementById('Depart').innerHTML=depart;
+    document.getElementById('arrived').innerHTML=arrived;
+    document.getElementById('seats').innerHTML=seats;
+    document.getElementById('level').innerHTML=level;
+    createTicketList(seats);
+
+
 }
 
+// 机票服务post提交
 function submitReserve() {
-    let tickets = $(this).find("ul[name='ticket']");
-    var ticketJson = [];
+    // let tickets = parseInt($('#toSeats').val());
+    let $tbody = document.getElementById("ticketTbody");
+    let tickets = $tbody.getElementsByTagName("tr");
+    // console.log(tickets.length);
+    let ticketJson = [];
+
     for (let i = 0; i < tickets.length; i++) {
-        var ticketSeat = tickets[i].find("li[name='ticketSeat']");
-        var ticketPrice = tickets[i].find("li[name='ticketPrice']");
-        ticketJson.push({"ticketSeat":ticketSeat, "ticketPrice":ticketPrice});
+        /* name*="xx" 表示获取所有包含name="xx"的dom */
+        let a = $('input[name*="ticketSeat"]')[i].value;
+        let b = $("input[name*='ticketPrice']")[i].value;
+        console.log(a+b);
+
+    //     // let a = document.getElementsByName("ticketSeat")[0].nodeValue;
+    //     let inputs = tickets[i].getElementsByTagName("td");
+    //     let ticketSeat = inputs[0].getElementsByName("ticketSeat").value;
+    //     let ticketPrice = inputs[1].getElementsByName("ticketSeat").value;
+    //     // let ticketPrice = inputs[1].firstElementChild;
+    //     console.log(ticketSeat);
+    //     console.log(ticketPrice);
+    //     // ticketJson.push({"ticketSeat":ticketSeat, "ticketPrice":ticketPrice});
+    //
     }
-
-
-    $.ajax({
-        data: {
-            "flightId": 1,
-            "companyId": 1,
-            "service": "test",
-            "tickets": ticketJson,
-        }
-    });
+    //
+    //
+    // $.ajax({
+    //     data: {
+    //         "flightId": 1,
+    //         "companyId": 1,
+    //         "service": "test",
+    //         "tickets": ticketJson,
+    //     }
+    // });
 }
+
 
 function createTicketList(seats) {
-    let $tbody = $('#ticketTbody');
+    let $tbody = document.getElementById("ticketTbody");
     let $ticket;
-    for (var i = 0; i < seats; i++) {
+    // console.log('去你的！'+seats);
+    for (let i = 0; i < seats; i++) {
         $ticket = document.createElement("tr");
-        var seat = document.createElement("td");
-        var price = document.createElement("td");
-        createInput('ticketSeat', 'text', "", seat);
-        createInput('ticketPrice', 'number', "", price);
+        const seat = document.createElement("td");
+        const price = document.createElement("td");
+        createInput('ticketSeat'+i, 'text', "", seat);
+        createInput('ticketPrice'+i, 'number', "", price);
         $ticket.appendChild(seat);
         $ticket.appendChild(price);
+        $tbody.appendChild($ticket);
     }
-    $tbody.appendChild($ticket);
-
 }
+
+function clearTicketList(){
+    $("#ticketTbody").empty();
+}
+
 function createInput(inputName, inputType, inputValue, aDiv) {
-    var input = document.createElement("input");
+    let input = document.createElement("input");
     input.setAttribute("type",inputType) ;
     input.setAttribute("name",inputName) ;
     input.setAttribute("value", inputValue) ;
+    // input.setAttribute("id", inputId) ;
+
     aDiv.appendChild(input);
 }
